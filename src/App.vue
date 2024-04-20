82f8a30a98e587ef431ee7d0dfeb5731a708bd83
Vitaly Tulin <me@tulvit.net>
2024-04-20 10:03:13 +0300

    content changing

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,10 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <HelloWorld msg="test assignment" />
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Settings</RouterLink>
+        <RouterLink to="/about">Order Book</RouterLink>
       </nav>
     </div>
   </header>
