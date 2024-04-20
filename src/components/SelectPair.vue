e958160a44e719209e5438d7e3309a16f0917c68
Vitaly Tulin <me@tulvit.net>
2024-04-20 15:58:02 +0300

    SelectPair component

--- src/components/SelectPair.vue
+++ src/components/SelectPair.vue
@@ -1,5 +1,21 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import { useLogStore } from '@/stores/log'
+
+const props = defineProps(['options'])
+const log = useLogStore()
+
+const selected = ref(props.options[0].value || '')
+
+watch(selected, (selected, prevSelected) => {
+  log.push(prevSelected, selected)
+})
+</script>
+
 <template>
-  <select>
-    <option>A</option>
+  <select v-model="selected">
+    <option v-for="(option, index) in props.options" :value="option.value" :key="index">
+      {{ option.text }}
+    </option>
   </select>
 </template>
