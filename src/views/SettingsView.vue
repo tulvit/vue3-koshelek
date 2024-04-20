c64ef784e1d3061cf9dba5c9a62414cb949aed7f
Vitaly Tulin <me@tulvit.net>
2024-04-20 14:32:13 +0300

    SelectingPair comonent

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import TheWelcome from '../components/TheWelcome.vue'
+import SelectPair from '../components/SelectPair.vue'
 </script>
 
 <template>
   <main>
     <h1>Settings</h1>
-    <TheWelcome />
+    <SelectPair />
+    <!-- <TheWelcome /> -->
   </main>
 </template>
