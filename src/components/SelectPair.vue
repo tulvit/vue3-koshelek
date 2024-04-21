67d2f5d6ae3747c7f60e82e7829efb9ef0d55d60
Vitaly Tulin <me@tulvit.net>
2024-04-21 13:20:54 +0300

    select pair refactoring

--- src/components/SelectPair.vue
+++ src/components/SelectPair.vue
@@ -1,14 +1,27 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { useLogStore } from '@/stores/log'
+import { useMarketDepthStore } from '@/stores/market'
+import { useSymbolStore } from '@/stores/symbol'
+import getDepth from '../comsolables/getDepth'
 
 const props = defineProps(['options'])
+const symbol = useSymbolStore()
 const log = useLogStore()
-
-const selected = ref(props.options[0].value || '')
+const marketDepth = useMarketDepthStore()
+const selected = ref(symbol.symbol.value || props.options[0].value)
+const { depth, error, load } = getDepth()
+load(selected.value)
+symbol.set({ value: selected.value, title: selected.value })
 
 watch(selected, (selected, prevSelected) => {
   log.push(prevSelected, selected)
+  load(selected)
+  symbol.set({ value: selected, title: selected })
+})
+
+watch(depth, () => {
+  marketDepth.save(depth.value)
 })
 </script>
 
