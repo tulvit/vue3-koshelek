ad56d1bf76cfde236ebb7711726f60f44e064320
Vitaly Tulin <me@tulvit.net>
2024-04-25 17:48:20 +0300

    REST marketDepth refactoring

--- src/stores/market.ts
+++ src/stores/market.ts
@@ -2,10 +2,41 @@ import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
 export const useMarketDepthStore = defineStore('marketDepth', () => {
-  const marketDepth = ref({})
+  const marketDepth = ref({ bids: {}, asks: {}, lastUpdateId: Number })
   function save(data: Object) {
-    marketDepth.value = data
+    marketDepth.value.bids = new Map(
+      data.bids.map((item) => {
+        return [item.shift(), item.shift()]
+      })
+    )
+    marketDepth.value.asks = new Map(
+      data.asks.map((item) => {
+        return [item.shift(), item.shift()]
+      })
+    )
+    marketDepth.value.lastUpdateId = data.lastUpdateId
   }
 
-  return { marketDepth, save }
+  function update(data: Object) {
+    data.b.map((value) => {
+      const price = value.shift()
+      const amount = value.shift()
+      if (amount > 0) marketDepth.value.bids.set(price, amount)
+      else marketDepth.value.bids.delete(price)
+      if (marketDepth.value.bids.size > 1100) {
+        marketDepth.value.bids.delete(marketDepth.value.bids.keys().next().value)
+      }
+    })
+    data.a.map((value) => {
+      const price = value.shift()
+      const amount = value.shift()
+      if (amount > 0) marketDepth.value.asks.set(price, amount)
+      else marketDepth.value.asks.delete(price)
+      if (marketDepth.value.asks.size > 1100) {
+        marketDepth.value.asks.delete(marketDepth.value.asks.keys().next().value)
+      }
+    })
+  }
+
+  return { marketDepth, save, update }
 })
